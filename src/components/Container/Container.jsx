5b45e8533256f69bb791e41e35495b9a9875930f
ajensen25 <aidenjensen707@gmail.com>
2025-03-23 11:06:08 -0400

    Added city input functionality

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,12 +1,28 @@
+import { useRef, useState } from "react";
 import "./Container.css";
 
 function Container() {
+  let [city, setCity] = useState("London");
+
+  const cityInput = useRef(null);
+
+  const onInputEnter = (e) => {
+    if (e.key === "Enter") {
+      setCity(cityInput.current.value.trim());
+    }
+  };
+
   return (
     <div className="container">
       <div className="city-input">
         <p>Enter a city name here.</p>
-        <input type="text" placeholder="London" />
-        <p className="city-display">London</p>
+        <input
+          type="text"
+          placeholder="London"
+          onKeyUp={onInputEnter}
+          ref={cityInput}
+        />
+        <p className="city-display">{city}</p>
       </div>
       <div className="weather-display">
         <div className="display-top">
